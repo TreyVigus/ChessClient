b430eb1ddc31eb344aa1726fe3f415bbd0b78df4
Trey Vigus <trey.vigus@sae.org>
2021-07-26 16:50:27 -0400

    make non existent directories in moveFiles

--- src/scripts/moveFiles.js
+++ src/scripts/moveFiles.js
@@ -1,6 +1,5 @@
 //This Script moves any files without .ts extensions from the app folder to the equivalent location of the build folder.
-//NOTE: This will NOT move directories, and thus expects the directory structure of the build/app folder to be the same as src/app,
-//			except possibly missing some files.
+//If an equivalent location in the build folder doesn't exist, this will create it.
 
 import readdirp from 'readdirp'; //allows for recursive traversal of directories. https://github.com/paulmillr/readdirp
 import fs from 'fs';
@@ -26,8 +25,37 @@ function extension(fileName) {
 //	copy them to the equivalent location in the build/app directory.
 function copyFiles(srcPaths) {
 	srcPaths.forEach(path => {
-		fs.copyFileSync(`src/app/${path}`, `build/app/${path}`);
+		const srcPath = `src\\app\\${path}`;
+		const destPath = `build\\app\\${path}`;
+		buildContainingDirectories(pathWithoutFile(destPath));
+		fs.copyFileSync(srcPath, destPath);
 	});
+}
+
+//e.g. src\app\thing.txt -> src\app
+function pathWithoutFile(path) {
+	const parts = path.split('\\');
+	parts.pop();
+	return parts.join('\\');
+}
+
+/**
+ * Suppose destWithoutFile = build\app\fake\anotherFake
+ * Suppose fake and anotherFake are directories that don't exist.
+ * This will create fake and anotherFake.
+ * That is, build\app\fake\anotherFake will be a valid path.
+ */
+function buildContainingDirectories(destWithoutFile) {
+	//check build, then build\app, then build\app\fake, then build\app\fake\anotherFake
+	//OPT: once a subpath is found that doesn't exist, all following subpaths won't exist
+	//OPT: could binary search for a subpath that doesn't exist
+	const parts = destWithoutFile.split('\\');
+	for(let i = 0; i < parts.length; i++) {
+		const subPath = parts.slice(0, i+1).join('\\');
+		if(!fs.existsSync(subPath)) {
+			fs.mkdirSync(subPath);
+		}
+	}
 }
 
 main();
