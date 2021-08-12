7daf5e1013527a3c6fbff50d7ee54e9f93f693fc
Trey Vigus <trey.vigus@sae.org>
2021-08-12 18:35:58 -0400

    make attack vectors module

---
+++ src/app/game/attackVectors.ts
@@ -0,0 +1,62 @@
+/** Return all squares in the same row as piecePos. */
+
+import { flat, posEquals } from "../utils/helpers.js";
+import { ChessState, Position, Square } from "./models.js";
+
+export function sameRow(piecePos: Position, state: ChessState): Square[] {
+    return state.board[piecePos[0]];
+}
+
+export function sameColumn(piecePos: Position, state: ChessState): Square[] {
+    let col: Square[] = [];
+    for(const item of flat(state.board)) {
+        if(item.index[1] === piecePos[1]) {
+            col.push(item.value);
+        }
+    }
+    return col;
+}
+
+export function samePositiveDiagonal(piecePos: Position, state: ChessState): Square[] {
+    //a positive diagonal consists of all squares with the same sum of coordinates.
+    const diagSum = piecePos[0] + piecePos[1];
+    return [...flat(state.board)].map(sq => sq.value).filter(s => (s.position[0] + s.position[1]) === diagSum);
+}
+
+export function sameNegativeDiagonal(piecePos: Position, state: ChessState): Square[] {
+    //a positive diagonal consists of all squares with the same difference of coordinates.
+    const diagDiff = piecePos[0] - piecePos[1];
+    return [...flat(state.board)].map(sq => sq.value).filter(s => (s.position[0] - s.position[1]) === diagDiff);
+}
+
+/** 
+ * Assume squares is either the row, col, or diagonal containing piecePos.
+ * Assume the piece at piecePos would attack all given squares if all squares were empty (no pieces present)
+ * Define leftBlocker as the first piece's pos to the left of piecePos in squares or 0 if none exists
+ * Define rightBlocker as the first piece's pos to the right of piecePos in squares or squares.length - 1 if none exists.
+ * Return squares[leftBlocker...rightBlocker].
+ */
+export function filterBlockedSquares(piecePos: Position, squares: Square[]): Square[] {
+    const piecePosIndex = squares.findIndex(s => posEquals(s.position, piecePos));
+    if(piecePosIndex === -1) {
+        throw 'piecePos was not found in squares array.'
+    }
+
+    let leftBlocker = 0;
+    for(let i = piecePosIndex - 1; i > -1; i--) {
+        if(squares[i].piece) {
+            leftBlocker = i;
+            break;
+        }
+    }
+
+    let rightBlocker = squares.length - 1;
+    for(let i = piecePosIndex + 1; i < squares.length; i++) {
+        if(squares[i].piece) {
+            rightBlocker = i;
+            break;
+        }
+    }
+
+    return squares.slice(leftBlocker, rightBlocker + 1);
+}
--- src/app/game/movements.ts
+++ src/app/game/movements.ts
@@ -1,6 +1,7 @@
 //Handles attacking squares, etc by piece
 import { clone, flat, itemAt, oppositeColor, posEquals, validPosition } from "../utils/helpers.js";
 import { BOARD_SIZE, MoveEvent } from "../view/boardView.js";
+import { filterBlockedSquares, sameColumn, sameNegativeDiagonal, samePositiveDiagonal, sameRow } from "./attackVectors.js";
 import { ChessState, Color, Piece, Position, Square } from "./models.js";
 import { classifyMove, PawnMoveType } from "./moveClassifier.js";
 
@@ -56,9 +57,6 @@ export function makeMove(precedingMove: MoveEvent | undefined, prevState: ChessS
 
     return copy;
 }
-
-
-
 
 /** Player cannot capture their own piece.  */
 function capturesOwnPiece(currentState: ChessState, attemptedMove: MoveEvent, piece: Piece): boolean {
@@ -194,69 +192,4 @@ function kingAttackedSquares(kingPos: Position, state: ChessState, king: Piece):
     ];
 
     return adjacent.filter(pos => validPosition(pos)).map(pos => itemAt(state.board, pos));
-}
-
-/** Return all squares in the same row as piecePos. */
-//TODO: remove export once parent function has testing.
-export function sameRow(piecePos: Position, state: ChessState): Square[] {
-    return state.board[piecePos[0]];
-}
-
-/** Return all squares in the same col as piecePos. */
-//TODO: remove export once parent function has testing.
-export function sameColumn(piecePos: Position, state: ChessState): Square[] {
-    let col: Square[] = [];
-    for(const item of flat(state.board)) {
-        if(item.index[1] === piecePos[1]) {
-            col.push(item.value);
-        }
-    }
-    return col;
-}
-
-//TODO: remove export once parent function has testing.
-export function samePositiveDiagonal(piecePos: Position, state: ChessState): Square[] {
-    //a positive diagonal consists of all squares with the same sum of coordinates.
-    const diagSum = piecePos[0] + piecePos[1];
-    return [...flat(state.board)].map(sq => sq.value).filter(s => (s.position[0] + s.position[1]) === diagSum);
-}
-
-//TODO: remove export once parent function has testing.
-export function sameNegativeDiagonal(piecePos: Position, state: ChessState): Square[] {
-    //a positive diagonal consists of all squares with the same difference of coordinates.
-    const diagDiff = piecePos[0] - piecePos[1];
-    return [...flat(state.board)].map(sq => sq.value).filter(s => (s.position[0] - s.position[1]) === diagDiff);
-}
-
-/** 
- * Assume squares is either the row, col, or diagonal containing piecePos.
- * Assume the piece at piecePos would attack all given squares if all squares were empty (no pieces present)
- * Define leftBlocker as the first piece's pos to the left of piecePos in squares or 0 if none exists
- * Define rightBlocker as the first piece's pos to the right of piecePos in squares or squares.length - 1 if none exists.
- * Return squares[leftBlocker...rightBlocker].
- */
-//TODO: remove export once parent function has testing.
-export function filterBlockedSquares(piecePos: Position, squares: Square[]): Square[] {
-    const piecePosIndex = squares.findIndex(s => posEquals(s.position, piecePos));
-    if(piecePosIndex === -1) {
-        throw 'piecePos was not found in squares array.'
-    }
-
-    let leftBlocker = 0;
-    for(let i = piecePosIndex - 1; i > -1; i--) {
-        if(squares[i].piece) {
-            leftBlocker = i;
-            break;
-        }
-    }
-
-    let rightBlocker = squares.length - 1;
-    for(let i = piecePosIndex + 1; i < squares.length; i++) {
-        if(squares[i].piece) {
-            rightBlocker = i;
-            break;
-        }
-    }
-
-    return squares.slice(leftBlocker, rightBlocker + 1);
 }
