066a31fb6fbdf782fb51b0bf1fb3b72f08efa2c2
Trey Vigus <trey.vigus@sae.org>
2021-08-14 11:28:18 -0400

    full initial state

--- src/app/game/main.ts
+++ src/app/game/main.ts
@@ -29,18 +29,36 @@ function initialState(): ChessState {
     board[0][2].piece = {color: 'black', name: 'bishop'};
     board[0][3].piece = {color: 'black', name: 'queen'};
     board[0][4].piece = {color: 'black', name: 'king'};
+    board[0][5].piece = {color: 'black', name: 'bishop'};
+    board[0][6].piece = {color: 'black', name: 'knight'};
+    board[0][7].piece = {color: 'black', name: 'rook'};
 
     board[1][0].piece = {color: 'black', name: 'pawn'};
     board[1][1].piece = {color: 'black', name: 'pawn'};
+    board[1][2].piece = {color: 'black', name: 'pawn'};
+    board[1][3].piece = {color: 'black', name: 'pawn'};
+    board[1][4].piece = {color: 'black', name: 'pawn'};
+    board[1][5].piece = {color: 'black', name: 'pawn'};
+    board[1][6].piece = {color: 'black', name: 'pawn'};
+    board[1][7].piece = {color: 'black', name: 'pawn'};
 
     board[6][0].piece = {color: 'white', name: 'pawn'};
     board[6][1].piece = {color: 'white', name: 'pawn'};
+    board[6][2].piece = {color: 'white', name: 'pawn'};
+    board[6][3].piece = {color: 'white', name: 'pawn'};
+    board[6][4].piece = {color: 'white', name: 'pawn'};
+    board[6][5].piece = {color: 'white', name: 'pawn'};
+    board[6][6].piece = {color: 'white', name: 'pawn'};
+    board[6][7].piece = {color: 'white', name: 'pawn'};
 
     board[7][0].piece = {color: 'white', name: 'rook'};
     board[7][1].piece = {color: 'white', name: 'knight'};
     board[7][2].piece = {color: 'white', name: 'bishop'};
     board[7][3].piece = {color: 'white', name: 'queen'};
     board[7][4].piece = {color: 'white', name: 'king'};
+    board[7][5].piece = {color: 'white', name: 'bishop'};
+    board[7][6].piece = {color: 'white', name: 'knight'};
+    board[7][7].piece = {color: 'white', name: 'rook'};
 
     return { board };
 }
