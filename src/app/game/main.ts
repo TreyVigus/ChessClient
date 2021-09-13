462d490629b8676462712581ef50ea3af7337611
Trey Vigus <trey.vigus@sae.org>
2021-09-13 18:19:20 -0400

    check highlighting

--- src/app/game/main.ts
+++ src/app/game/main.ts
@@ -1,7 +1,8 @@
-import { constructBoard, flat } from "../utils/helpers.js";
+import { constructBoard, flat, posSequence } from "../utils/helpers.js";
 import { BoardView, initView, MoveEvent } from "../view/boardView.js";
 import { ChessState, Position, Square } from "./models.js";
 import { isLegal, makeMove } from "./movements.js";
+import { findKing, inCheck } from "./stateQueries.js";
 
 /********* Debugging flags **********/
 const showSquarePositions = false; //render simple board with position info
@@ -39,6 +40,24 @@ function drawState(state: ChessState, view: BoardView) {
         if(s.value.piece) {
             view.drawPiece(s.value.piece, s.index);
         }
+    }
+    highlightInCheck(state, view);
+}
+
+function highlightInCheck(state: ChessState, view: BoardView) {
+    //clear out any previous highlighting
+    posSequence().forEach(pos => {
+        view.removeHighlight(pos);
+    });
+
+    const whiteKing = findKing(state, 'white').position;
+    if(inCheck(state, 'white')) {
+        view.addHighlight(whiteKing);
+    }
+
+    const blackKing = findKing(state, 'black').position;
+    if(inCheck(state, 'black')) {
+        view.addHighlight(blackKing);
     }
 }
 
