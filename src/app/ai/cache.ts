58e59db5ec04928da3e434dce2cd159602ca36b0
Trey Vigus <trey.vigus@sae.org>
2021-11-08 16:26:54 -0500

    fast cache

--- src/app/ai/cache.ts
+++ src/app/ai/cache.ts
@@ -42,23 +42,27 @@ export function getEmptyCache(): EvalCache {
 
 /** 
  * OPT: could make this return a unique integer for a state. 
+ * currently this returns ~ 383 character string
+ * can represent any square with a unique number
+ * need to find a way to 'build' a hash, square by square
+ * https://en.wikipedia.org/wiki/Rolling_hash
  * @todo unit test
  * */
 function serialize(state: ChessState): string {
-    let ser = "";
-    posSequence().forEach(pos => {
+    let ser: string[] = new Array(64);
+    posSequence().forEach((pos, index) => {
         const sq = itemAt(state.board, pos);
-        ser = `${ser}~${serializeSquare(sq)}`
+        ser[index] = serializeSquare(sq);
     });
-    return ser;
+    return ser.join('');
 }
 
 function serializeSquare(s: Square): string {
-    return `${serializePiece(s.piece)}.${serializeTouched(s.touched)}`;
+    return `${serializePiece(s.piece)}${serializeTouched(s.touched)}`;
 }
 
 function serializePiece(p: Square['piece']): string {
-    return p ? `${p.color},${p.name}` : 'none';
+    return p ? `${p.name}${p.color}` : '00';
 }
 
 function serializeTouched(touched: Square['touched']): string {
